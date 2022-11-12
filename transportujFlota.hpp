dc0a80b3b912c98b58a297487a208b2fd099f855
JWachnicki <julek.wachnicki@wp.pl>
2022-11-12 23:39:48 +0100

    Chyba powinno działać

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,30 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    //Warunki na początku
+    unsigned int ilosc_lodek = 0; //Taki int ale dodatni i może mieć większą wartośc niż zwykły
+    unsigned int ilosc_towaru = 0;
+    if (towar == 0) 
+    {
+        return 0;
+    }
+    else
+    {
+        while (ilosc_towaru < towar)
+        {
+            //1. Stworzenie statku
+            Stocznia stocznia{};
+            Statek* s1 = stocznia();
+            //2. Wywołanie metody transportuj
+            przetransportowany = przetransportowany + s1->transportuj();
+            //3. Sumowanie łódek
+            if (s1 == dynamic_cast<Zaglowiec*>(s1))
+            {
+                ilosc_lodek++;
+            }
+            //4. Usunięcie łódki
+            delete s1;
+        }
+        //5. Powrót do 1. jeżeli nie został spełniony warunek ilości towaru
+    }
 }
